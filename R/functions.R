4054777baa40d9abe08b0cf590ff426b1bcff9de
Joshua Rosenberg <jmrosen48@gmail.com>
2019-04-14 10:25:51 -0400

    start to add code to process edgelist

--- R/functions.R
+++ R/functions.R
@@ -3,9 +3,8 @@
 
 ## https://github.com/jennybc/googlesheets
 
-# b_url <- "https://docs.google.com/spreadsheets/d/1s5Zyd0u_Y0GLooNgQDUanXZDACZxdnc4fHc5wgZ6JE0/edit#gid=8743918"
-# file <- "siteconf.csv"
-j_url <- "https://docs.google.com/spreadsheets/d/1WM2xWG9B0Wqn3YG5uakfy_NSAEzIFP2nEAJ5U_fqufc/edit#gid=8743918"
+#b_url <- "https://docs.google.com/spreadsheets/d/1s5Zyd0u_Y0GLooNgQDUanXZDACZxdnc4fHc5wgZ6JE0/edit#gid=8743918"
+# j_url <- "https://docs.google.com/spreadsheets/d/1WM2xWG9B0Wqn3YG5uakfy_NSAEzIFP2nEAJ5U_fqufc/edit#gid=8743918"
 
 read_tags <- function(url) {
     u <- googlesheets::gs_url(url)
@@ -14,13 +13,35 @@ read_tags <- function(url) {
 }
 
 
-pull_data <- function(df, n) {
+pull_data <- function(df, n = NULL) {
+    if(is.null(n)) n <- nrow(df)
     split_status <- stringr::str_split(df$status_url, "/")
     status_id <- purrr::map(split_status, ~ .[[6]])
     status_id <- unlist(status_id)
     d <- rtweet::lookup_statuses(status_id[1:n])
     d
 }
+
+create_edgelist <- function() {
+
+}
+
+reply_tweets <- df_ss %>%
+        filter(type=="REPLY") %>%
+        count(reply_to_status_id) %>%
+        rename(replies_count = n)
+
+d <- d %>%
+        mutate(mentions_count = sapply(mentions_screen_name, length),
+               hashtags_count = sapply(str_split(hashtags, " "), length),
+               urls_count = sapply(str_split(urls_url, " "), length),
+               is_reply = if_else(!is.na(reply_to_status_id), 1, 0))
+
+prepped_data <- df_ss %>%
+        filter(type=="ORIG") %>%
+        mutate(reply_to_status_id = status_id) %>%
+        left_join(reply_tweets, by = "reply_to_status_id") %>%
+        mutate(replies_count = ifelse(is.na(replies_count), 0, replies_count)) %>%
 
 ## --------------------------------------------------------------
 ## pull full tweet content using library(rtweet)
