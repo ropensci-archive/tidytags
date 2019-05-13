56b7ed75a52664b772507f4dd7514e4a790a4e6d
Joshua Rosenberg <jmrosen48@gmail.com>
2019-05-13 09:36:08 -0400

    update

--- R/functions.R
+++ R/functions.R
@@ -26,92 +26,24 @@ pull_data <- function(df, n = NULL) {
   d
 }
 
+length_with_na <- function(x) {
+  ifelse(is.na(x), 0, map_int(x, length))
+}
+
 proc_tweets <- function(d) {
   d %>%
-    mutate(mentions_count = map_int(mentions_screen_name, length),
-           hashtags_count = map_int(str_split(as.character(hashtags), " "), length),
-           urls_count = map_int(str_split(as.character(urls_url), " "), length),
+    mutate(mentions_count = length_with_na(mentions_screen_name),
+           hashtags_count = length_with_na(hashtags),
+           urls_count = length_with_na(urls_url),
            is_reply = if_else(!is.na(reply_to_status_id), TRUE, FALSE))
 }
 
-# Replies
-
-get_replies <- function(d) {
-  reply_tweets <- d %>%
-    filter(is_reply == 1) %>%
-    count(screen_name, status_id, reply_to_status_id) %>%
-    select(sender = screen_name, replies_count = n, reply_status_id = status_id, status_id = reply_to_status_id)
-
-  replied_to_tweets <- lookup_statuses(reply_tweets$status_id)
-  replied_to_tweets <- proc_tweets(replied_to_tweets)
-
-  replied_to_tweets %>%
-    left_join(reply_tweets, by = c("status_id")) %>%
-    select(sender, receiver = screen_name) %>%
-    mutate(edge_type = "reply")
-}
-
-# Retweets
-
-get_retweets <- function(d) {
+proc_tweets_flattened <- function(d) {
   d %>%
-    filter(is_retweet) %>%
-    select(sender = screen_name, receiver = retweet_screen_name) %>%
-    mutate(edge_type = "retweet")
+    mutate(mentions_count = length_with_na(str_split(mentions_screen_name, " ")),
+           hashtags_count = length_with_na(str_split(hashtags, " ")),
+           urls_count = length_with_na(str_split(urls_url, " ")),
+           is_reply = if_else(!is.na(reply_to_status_id), TRUE, FALSE))
 }
 
-
-# Favorites
-
-# dd %>%
-#   select(status_id, screen_name)
-#
-# ex_favs <- get_favorites(user = d[1, ]$screen_name,
-#               max_id = d[1, ]$status_id)
-#
-# ex_favs$status_id %in% d[1, ]$status_id
-# View(ex_favs)
-#
-# list(status_id = dd$status_id, screen_name = dd$screen_name) %>%
-#   map2(.f = get_favorites())
-
-# Quotes
-
-get_quotes <- function(d) {
-  d %>%
-    filter(is_quote) %>%
-    select(sender = screen_name, receiver  =quoted_screen_name) %>%
-    mutate(edge_type = "quotes")
-}
-
-
-# Mentions
-
-get_mentions <- function(d) {
-  d %>%
-    select(screen_name, mentions_screen_name) %>%
-    unnest() %>%
-    rename(sender = screen_name, receiver = mentions_screen_name) %>%
-    mutate(edge_type = "mention")
-}
-
-# Create edgelist
-
-create_edgelist <- function(d) {
-
-  dd <- proc_tweets(d)
-
-  # these can be processed from the data
-  retweet_edges <- get_retweets(dd)
-  quote_edges <- get_quotes(dd)
-  mention_edges <- get_mentions(dd)
-
-  # these require additional API calls
-  reply_edges <- get_replies(dd)
-  # favorite_edges <- get_favorites(dd)
-
-  bind_rows(retweet_edges,
-            quote_edges,
-            mention_edges,
-            reply_edges)
-}
+#proc_tweets(d) %>% select(urls_url, urls_count) %>% View()
