a4c5cb58802495805a5c00293d2524cad03f1257
Bret Staudt Willet <bret.staudt.willet@gmail.com>
2020-02-05 13:02:31 -0500

    added function to pull upstream replies

---
+++ R/add-upstream-replies.R
@@ -0,0 +1,24 @@
+get_upstream_replies <- function(df) {
+  processed_df <- process_tweets(df)
+  unknown_replies <- dplyr::filter(processed_df,
+                                   is_reply & (reply_to_status_id %notin% processed_df$status_id)
+                                   )
+  searchable_replies <- nrow(pull_tweet_data(unknown_replies$reply_to_status_id))
+  i <- 0
+
+  while(searchable_replies > 0) {
+    i = i + 1
+    print(paste("Iteration:", i))
+    new_tweets <- process_tweets(pull_tweet_data(unknown_replies$reply_to_status_id))
+    processed_df <- rbind(processed_df, new_tweets)
+    unknown_replies <- dplyr::filter(processed_df,
+                                     is_reply & (reply_to_status_id %notin% processed_df$status_id)
+                                     )
+    searchable_replies <- nrow(pull_tweet_data(unknown_replies$reply_to_status_id))
+    print(paste0("New tweets added: ", nrow(new_tweets),
+                 "; Unknown replies:", nrow(unknown_replies),
+                 "; Searchable replies: ", searchable_replies))
+  }
+
+  processed_df
+}
--- R/setup-functions.R
+++ R/setup-functions.R
@@ -3,6 +3,8 @@
 
 
 `%notin%` <- Negate(`%in%`)
+url_regex <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+hashtag_regex <- "#([0-9]|[a-zA-Z])+"
 
 
 read_tags <- function(url) {
@@ -12,25 +14,23 @@ read_tags <- function(url) {
 }
 
 
-get_char_tweet_ids <- function(x) {
-  split_tweet_url <- stringr::str_split(x, "/")
-  char_ids <- purrr::map(split_tweet_url, ~ .[[6]])
-  char_ids <- unlist(char_ids)
-  distinct_char_ids <- unique(char_ids)
-  distinct_char_ids
+get_char_tweet_ids <- function(df) {
+  df <- dplyr::mutate(df,
+                status_id_char = stringr::str_split_fixed(status_url, "/", n=6)[ ,6]
+                )
+  dplyr::pull(df, status_id_char)
 }
 
 
-pull_tweet_data <- function(df, n = NULL) {
-  if(is.null(n)) {n <- nrow(df)}
-  tweet_ids <- get_char_tweet_ids(df$status_url)
-  if (length(tweet_ids) > 90000) {
+pull_tweet_data <- function(x, n = NULL) {
+  if(is.null(n)) {n <- length(x)}
+  if (length(x) > 90000) {
     warning("Twitter API only allows lookup of 90,000 tweets at a time;",
             "collecting data for first 90,000 tweet IDs.",
             "To process more, use `lookup_many_tweets()`.")
-    tweet_ids <- tweet_ids[1:90000]
+    x <- x[1:90000]
   }
-  new_df <- rtweet::lookup_statuses(tweet_ids[1:n])
+  new_df <- rtweet::lookup_statuses(x[1:n])
   new_df
 }
 
@@ -40,7 +40,7 @@ lookup_many_tweets <- function(df, alarm = FALSE) {
   new_df <- data.frame()
   for(i in 1:n_batches) {
     min_id <- 90000*i - 89999
-    max_id <- ifelse(90000*i < nrow(df), 90000*i, nrow(df))
+    max_id <- dplyr::if_else(90000*i < nrow(df), 90000*i, nrow(df))
     tmp_df <- pull_tweet_data(df[min_id:max_id, ]) #purrr::flatten() %>%
     new_df <- rbind(new_df, tmp_df)
     if(alarm == TRUE) {beepr::beep(2)}
@@ -52,15 +52,20 @@ lookup_many_tweets <- function(df, alarm = FALSE) {
 
 
 length_with_na <- function(x) {
-  ifelse(is.na(x), 0, purrr::map_int(x, length))
+  dplyr::if_else(is.na(x), 0, purrr::map_int(x, length))
 }
 
 
 process_tweets <- function(df) {
   dplyr::mutate(df,
+                word_count = stringr::str_count(text, "\\s+") + 1,
+                character_count = stringr::str_length(text),
                 mentions_count = length_with_na(mentions_screen_name),
-                hashtags_count = length_with_na(hashtags),
-                urls_count = length_with_na(urls_url),
+                hashtags_count_api = length_with_na(hashtags),
+                hashtags_count_regex = stringr::str_count(text, hashtag_regex),  # more accurate than API
+                has_hashtags = dplyr::if_else(hashtags_count_regex != 0, TRUE, FALSE),
+                urls_count_api = length_with_na(urls_url),
+                urls_count_regex = stringr::str_count(text, url_regex),  # counts links to quoted tweets and media
                 is_reply = dplyr::if_else(!is.na(reply_to_status_id), TRUE, FALSE),
                 is_self_reply = dplyr::if_else(is_reply,
                                               dplyr::if_else(user_id==reply_to_user_id,
@@ -74,9 +79,14 @@ process_tweets <- function(df) {
 
 process_tweets_flattened <- function(df) {
   dplyr::mutate(df,
+                word_count = stringr::str_count(text, "\\s+") + 1,
+                character_count = stringr::str_length(text),
                 mentions_count = length_with_na(str_split(mentions_screen_name, " ")),
-                hashtags_count = length_with_na(str_split(hashtags, " ")),
-                urls_count = length_with_na(str_split(urls_url, " ")),
+                hashtags_count_api = length_with_na(str_split(hashtags, " ")),
+                hashtags_count_regex = stringr::str_count(text, hashtag_regex),  # more accurate than API
+                has_hashtags = dplyr::if_else(hashtags_count_regex != 0, TRUE, FALSE),
+                urls_count_api = length_with_na(str_split(urls_url, " ")),
+                urls_count_regex = stringr::str_count(text, url_regex),  # counts links to quoted tweets and media
                 is_reply = dplyr::if_else(!is.na(reply_to_status_id), TRUE, FALSE),
                 is_self_reply = dplyr::if_else(is_reply,
                                               dplyr::if_else(user_id==reply_to_user_id,
