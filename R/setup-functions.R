b9cc8c6acd5caa614435e06e43195974f0394820
Bret Staudt Willet <bret.staudt.willet@gmail.com>
2020-02-11 13:05:04 -0500

    added additional documentation

--- R/setup-functions.R
+++ R/setup-functions.R
@@ -1,11 +1,25 @@
-# Read more about library(googlesheets) at https://github.com/jennybc/googlesheets
+#' Retrieve a TAGS archive of tweets and bring into R
+#'
+#' @param url A valid URL (i.e., hyperlink) to a TAGS tracker
+#' @return A dataframe of the TAGS archive of tweets
+#' @seealso Read more about \code{library(googlesheets)} \href{https://github.com/jennybc/googlesheets}{here}.
 read_tags <- function(url) {
   full_workbook <- googlesheets::gs_url(url)
   one_sheet <- googlesheets::gs_read(full_workbook, ws = 2)
   one_sheet
 }
 
-
+#' Get tweet ID numbers as character strings
+#'
+#' This function is useful because Google Sheets (and hence TAGS)
+#'   typically round very large numbers into an exponential form. Thus,
+#'   because tweet ID numbers are very large, they often get corrupted in this
+#'   rounding process. The most reliable way to get full tweet ID numbers is by
+#'   using this function, \code{get_char_tweet_ids()}, to pull the ID numbers from
+#'   the URL linking to specific tweets.
+#' @param df A dataframe of containing the column name 'status_url'
+#'   (i.e., the hyperlink to specific tweets)
+#' @return A list or vector of tweet IDs as character strings
 get_char_tweet_ids <- function(df) {
   df <- dplyr::mutate(df,
                 status_id_char = stringr::str_split_fixed(status_url, "/", n=6)[ ,6]
@@ -13,8 +27,13 @@ get_char_tweet_ids <- function(df) {
   dplyr::pull(df, status_id_char)
 }
 
-
-## Read more about library(rtweet) at https://rtweet.info/
+#' Retrieve the fullest extent of tweet metadata available from the Twitter API
+#'
+#' @param x A list or vector of tweet ID numbers
+#' @param n The number of tweets to look up, by default the total number of tweet
+#'   ID numbers available, but capped at 90,000 due to Twitter API limitations.
+#' @return A dataframe of tweets and full metadata from the Twitter API
+#' @seealso Read more about \code{library(rtweet)} \href{https://rtweet.info/}{here}.
 pull_tweet_data <- function(x, n = NULL) {
   if(is.null(n)) {n <- length(x)}
   if (length(x) > 90000) {
@@ -27,14 +46,21 @@ pull_tweet_data <- function(x, n = NULL) {
   new_df
 }
 
-
-lookup_many_tweets <- function(df, alarm = FALSE) {
-  n_batches <- ceiling(nrow(df) / 90000)
+#' Retrieve the fullest extent of tweet metadata for more than 90,000 tweets
+#'
+#' This function calls \code{pull_tweet_data()}, but has a built in delay
+#'   of 15 minutes to all the Twitter API to reset after looking up 90,000 tweets.
+#' @param x A list or vector of tweet ID numbers
+#' @param alarm An audible notification that a batch of 90,000 tweets has been
+#'   completed
+#' @return A dataframe of tweets and full metadata from the Twitter API
+lookup_many_tweets <- function(x, alarm = FALSE) {
+  n_batches <- ceiling(length(x) / 90000)
   new_df <- data.frame()
   for(i in 1:n_batches) {
     min_id <- 90000*i - 89999
-    max_id <- dplyr::if_else(90000*i < nrow(df), 90000*i, nrow(df))
-    tmp_df <- pull_tweet_data(df[min_id:max_id, ]) #purrr::flatten() %>%
+    max_id <- dplyr::if_else(90000*i < length(x), 90000*i, length(x))
+    tmp_df <- pull_tweet_data(x[min_id:max_id])
     new_df <- rbind(new_df, tmp_df)
     if(alarm == TRUE) {beepr::beep(2)}
     if(n_batches > 1) {message("Processed batch: ", i)}
@@ -43,12 +69,20 @@ lookup_many_tweets <- function(df, alarm = FALSE) {
   new_df
 }
 
-
+#' Count the number of items in a list, with an NA entry returning 0
+#'
+#' @param x A list
+#' @return The number of items in the list
 length_with_na <- function(x) {
   ifelse(is.na(x), 0, purrr::map_int(x, length))
 }
 
-
+#' Calculate additional information using tweet metadata
+#'
+#' @param df A dataframe of tweets and full metadata from the Twitter API
+#' @return A dataframe with several additional columns: word_count, character_count,
+#'   mentions_count, hashtags_count_api, hashtags_count_regex, has_hashtags,
+#'   urls_count_api, urls_count_regex, is_reply, is_self_reply
 process_tweets <- function(df) {
   url_regex <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
   hashtag_regex <- "#([0-9]|[a-zA-Z])+"
@@ -71,7 +105,12 @@ process_tweets <- function(df) {
                 )
 }
 
-
+#' Calculate additional information using tweet metadata
+#'
+#' @param df A dataframe of tweets and full metadata from the Twitter API
+#' @return A dataframe with several additional columns: word_count, character_count,
+#'   mentions_count, hashtags_count_api, hashtags_count_regex, has_hashtags,
+#'   urls_count_api, urls_count_regex, is_reply, is_self_reply
 process_tweets_flattened <- function(df) {
   url_regex <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
   hashtag_regex <- "#([0-9]|[a-zA-Z])+"
