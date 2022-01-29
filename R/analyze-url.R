5c8c657ee8fcc71ff4ba3ed54cc97f8115ad0578
Bret Staudt Willet <bret.staudt.willet@gmail.com>
2022-01-29 14:08:53 -0500

    used roxygen2md to clean up documentation

--- R/analyze-url.R
+++ R/analyze-url.R
@@ -1,17 +1,17 @@
 #' Find the domain name of URLs, even shortened URLs
 #'
-#' \code{get_url_domain()} retrieves the Web domain name from a URL, including
+#' `get_url_domain()` retrieves the Web domain name from a URL, including
 #'   URLs shortened with services such as bit.ly and t.co
 #' @param x A list or vector of hyperlinks, whether shortened or expanded
 #' @param wait How long (in seconds) to wait on the
-#'   \code{longurl::expand_urls()} function to retrieve the full, expanded URL
-#'   from a shortened URL (e.g., a bit.ly). The \code{longurl} default is 2
+#'   `longurl::expand_urls()` function to retrieve the full, expanded URL
+#'   from a shortened URL (e.g., a bit.ly). The `longurl` default is 2
 #'   seconds, but we have found that this misses a number of valid URLs. Here,
-#'   we have made the default \code{wait = 10} seconds, but the user can adjust
+#'   we have made the default `wait = 10` seconds, but the user can adjust
 #'   this as they like.
 #' @return A list or vector of Web domain names
-#' @seealso Read the documentation for \code{longurl::expand_urls()} and
-#'   \code{urltools::domain()}.
+#' @seealso Read the documentation for `longurl::expand_urls()` and
+#'   `urltools::domain()`.
 #' @examples
 #'
 #' \dontrun{
